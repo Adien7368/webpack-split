361c6e48cc2557a25c7cbb317e27959e70b90b5c
Avinash Verma <avinashverma@Avinashs-MacBook-Pro.local>
2023-05-29 04:06:30 +0530

    removing unwanted config

--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,6 @@ module.exports = {
   entry: './src/index.js',
   optimization: {
     minimizer: [],
-    splitChunks: {
-      cacheGroups: {
-        presto: {
-          test(module) {
-            // `module.resource` contains the absolute path of the file on disk.
-            // Note the usage of `path.sep` instead of / or \, for cross-platform compatibility.
-            // console.log(module)
-            console.log(module.resource, module.resource.includes('PrestoDOM'))
-            return module.resource.includes('PrestoDOM');
-          },
-
-          chunks: 'initial',
-          name: 'prestodom',
-          chunks: 'all',
-        },
-        qr_generator: {
-          test(module) {
-            // `module.resource` contains the absolute path of the file on disk.
-            // Note the usage of `path.sep` instead of / or \, for cross-platform compatibility.
-            // console.log(module)
-            console.log(module.resource, module.resource.includes('QR-Generator'))
-            return module.resource.includes('QR-Generator');
-          },
-
-          chunks: 'initial',
-          name: 'qr_generator',
-          chunks: 'all',
-        }
-      }
-    }
    },
   output: {
     filename: 'main.js',
